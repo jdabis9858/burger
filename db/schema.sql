53232b1f37aecbc111f7154a682376c5804b2f0d
jdabis9858 <jdabis4281994@gmail.com>
2019-03-11 14:37:57 -0700

    final edits

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,14 @@
-DROP DATABASE IF EXISTS burger_db;
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+
+DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
-    id INTEGER(30) AUTO_INCREMENT NOT NULL,
-    burger VARCHAR(30) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
 );
 
  SELECT * FROM burgers;
